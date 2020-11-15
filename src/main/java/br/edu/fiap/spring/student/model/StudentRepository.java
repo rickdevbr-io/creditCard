872328a8e076425953d148c109aeccbe1220797c
ricardodeveloper <ricardodeveloper21br@gmail.com>
2020-11-15 05:50:00 -0300

    adição swagger

--- src/main/java/br/edu/fiap/spring/student/model/StudentRepository.java
+++ src/main/java/br/edu/fiap/spring/student/model/StudentRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long>, JpaSpecificationExecutor<Student>{
 
 	List<Student> findAll();
